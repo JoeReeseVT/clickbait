06f5a9726e9aa1936b50bf853a14d3cc8584ae0f
JDSeid <72578336+JDSeid@users.noreply.github.com>
2020-10-17 11:23:52 -0400

    Timer Script
    This script is given to the player. Create a textmeshpro object in the hierarchy. Drag the TMP object to the script in the inspector.

---
+++ TimerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+//using System.Math;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI timerText;
+    int intTime;
+    private double time = 0;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        intTime = (int)time;
+        
+        timerText.text = intTime.ToString();
+    }
+}
