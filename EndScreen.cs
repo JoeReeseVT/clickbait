e0025c494b253cb36669fd54c24c4745f418b83e
maxbehrendt1 <70982369+maxbehrendt1@users.noreply.github.com>
2020-10-18 13:37:52 -0400

    Triggers the end game screen

---
+++ EndScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject Endscrn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Endscrn.gameObject.SetActive(true);
+        }
+    }
+}
